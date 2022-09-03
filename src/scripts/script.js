3c7dcc6f8076ff1d8a9e7fade21be84aeb122674
veyronB2 <lokyt2@gmail.com>
2022-09-03 12:58:14 +0100

    javascript updated to reflect new HTML

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -30,58 +30,52 @@ function stateControl() {
 function Renderer() {
   function renderProducts(state) {
     const mainBodyProductsContainer = selectors.getProductsContainer();
-    console.log(mainBodyProductsContainer);
+
     if (!mainBodyProductsContainer.firstChild) {
       removeProducts();
     }
 
     state.products.forEach((product) => {
-      const productItem = document.createElement("div");
-      productItem.classList.add("mainbody__product-item");
+      const productContainer = document.createElement("div");
+      productContainer.classList.add("mainbody__product-item");
 
-      const productImgContainer = document.createElement("div");
-      productImgContainer.classList.add("product-img");
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("product-img");
 
-      const productImage = document.createElement("img");
-      productImage.src = product.image;
-      productImage.alt = product.type;
+      const image = document.createElement("img");
+      image.src = product.image;
+      image.alt = product.name;
 
-      const productDetailsWarpper = document.createElement("div");
-      productDetailsWarpper.classList.add("product-details-wrapper");
+      const productTextContainer = document.createElement("div");
+      productTextContainer.classList.add("product-text");
 
-      const productDetails = document.createElement("div");
-      productDetails.classList.add("product-details");
+      const productH2 = document.createElement("h2");
+      productH2.innerText = product.name;
+      const productH4 = document.createElement("h4");
+      productH4.innerText = product.colour;
+      const productDesc = document.createElement("p");
+      productDesc.innerText = product.desc;
 
-      const productHeader = document.createElement("h2");
-      productHeader.innerText = product.name;
+      const shopBtnContainer = document.createElement("div");
+      shopBtnContainer.classList.add("shop-btn");
+      const productButtonContainer = document.createElement("div");
+      productButtonContainer.classList.add("btn");
+      productButtonContainer.classList.add("product-btn");
 
-      const productColour = document.createElement("h4");
-      productColour.innerText = product.colour;
+      const productLink = document.createElement("a");
+      productLink.href = "#";
+      productLink.role = `shop ${product.name}`;
+      productLink.innerText = `shop ${product.name}`;
 
-      const productDescContiner = document.createElement("div");
-      productDescContiner.classList.add("product-desc");
+      productButtonContainer.appendChild(productLink);
+      shopBtnContainer.appendChild(productButtonContainer);
 
-      const productText = document.createElement("p");
-      productText.innerText = product.desc;
+      productTextContainer.append(productH2, productH4, productDesc);
 
-      const button = document.createElement("a");
-      button.href = "#";
-      button.role = `shop ${product.name}`;
-      button.innerText = `shop ${product.name}`;
-      const buttonContainer = document.createElement("div");
-      buttonContainer.classList.add("btn");
-      buttonContainer.classList.add("product-btn");
+      imageContainer.append(image, productTextContainer);
+      productContainer.append(imageContainer, shopBtnContainer);
 
-      buttonContainer.appendChild(button);
-
-      productImgContainer.appendChild(productImage);
-      productDescContiner.appendChild(productText);
-      productDetails.append(productHeader, productColour, productDescContiner);
-
-      productDetailsWarpper.append(productDetails, buttonContainer);
-      productItem.append(productImgContainer, productDetailsWarpper);
-
-      mainBodyProductsContainer.appendChild(productItem);
+      mainBodyProductsContainer.appendChild(productContainer);
     });
   }
 
